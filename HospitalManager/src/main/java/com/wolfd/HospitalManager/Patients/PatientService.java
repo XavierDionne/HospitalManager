43f6f1f492a5c869d67f7670d2b7e912dee9f44f
BruceL <BruceL@WIN10PF19WCHK.corp.resmed.org>
2021-10-20 10:47:45 -0300

    removed whitespace in all files to introduce a change to create a pull request.

--- HospitalManager/src/main/java/com/wolfd/HospitalManager/Patients/PatientService.java
+++ HospitalManager/src/main/java/com/wolfd/HospitalManager/Patients/PatientService.java
@@ -3,13 +3,14 @@
  */
 package com.wolfd.HospitalManager.Patients;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
